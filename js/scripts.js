9ef029e873f27964145b02a8dae48f58a2d2cee7
Isaac Hall, Nuala Gray and Tate Easterbrooks <isaac@>
2019-01-16 15:19:46 -0800

    adds the entire program. Javascript input forms for job application

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
+function popularity(fage,ffollow){
+  return ffollow/fage;
+}
+
+
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
     var ageInput = $("input#age").val();
     var ethnicityInput= $("input#ethnicity").val();
     var followersInput = $("input#followers").val();
-    var beardInput = $("input#beard").val();
+    var beardInput = $("select#beard").val();
     var singleInput = $("input#single").val();
+    var pAge = parseInt(ageInput);
+    var pFollowers = parseInt(followersInput);
+
 
     $(".name").text(nameInput);
-    $(".age").text(ageInput);
+    $(".age").text(pAge);
     $(".ethnicity").text(ethnicityInput);
-    $(".followers").text(followersInput);
+    $(".followers").text(pFollowers);
     $(".beard").text(beardInput);
-    $(".single").text("singleInput");
+    $(".single").text(singleInput);
 
     $("#story").show();
     event.preventDefault();
+
+    var pop = popularity (pAge,pFollowers);
+
+
+    $(".pop").text(pop);
+
   });
+
+
+
 });
